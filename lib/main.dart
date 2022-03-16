943fce8350d7798bafb1375fed921be0a6f85892
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-16 19:03:43 +0300

    Flutter Cards Practice

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,9 @@ class MyApp extends StatelessWidget {
                 color: Colors.white,
                 child: Text("Card 1"),
               ),
+              SizedBox(
+                height: 20.0,
+              ),
               Container(
                 height: 100.0,
                 width: 100.0,
