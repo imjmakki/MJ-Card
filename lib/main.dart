f46d9f4c8f0e9a588c81cd2c6d6ebc6084b820f1
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-16 18:45:00 +0300

    Flutter Cards Practice

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            mainAxisSize: MainAxisSize.min,
             children: [
               Container(
                 height: 100.0,
