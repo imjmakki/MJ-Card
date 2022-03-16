8d7a40887cda5bd899f86976bdca16b78360ae26
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-16 19:46:24 +0300

    Flutter Cards Practice

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
                   fontSize: 40.0,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
                 ),
               ),
             ],
