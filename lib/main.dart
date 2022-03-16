5bea469fa370e3b701dcbb73377dee0529c2c233
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-16 19:24:52 +0300

    Flutter Cards Practice

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
             children: [
               CircleAvatar(
                 radius: 50.0,
-                backgroundColor: Colors.amberAccent,
+                backgroundImage: ,
               ),
             ],
           ),
