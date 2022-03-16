102667903416b4881fba34e018fa5869e87e465f
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-16 19:13:03 +0300

    Flutter Cards Practice

--- lib/main.dart
+++ lib/main.dart
@@ -13,32 +13,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.white,
-                child: Text("Card 1"),
-              ),
-              SizedBox(
-                width: 20.0,
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.blue,
-                child: Text("Card 2"),
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.amber,
-                child: Text("Card 3"),
-              ),
-            ],
-          ),
+          child: Column(),
         ),
       ),
     );
