a6e6d3f70e261324b3f0dcb5e13c46765b422cef
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-16 18:53:05 +0300

    Flutter Cards Practice

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Container(
                 height: 100.0,
