21d3a6904a24ead490c473d936eb9a2d34149ecc
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-17 14:45:18 +0300

    Flutter Cards Practice

--- lib/main.dart
+++ lib/main.dart
@@ -13,79 +13,73 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: AssetImage('images/1.jpeg'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/1.jpeg'),
+              ),
+              Text(
+                'Mohamad J. Makki',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
-                Text(
-                  'Mohamad J. Makki',
-                  style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+              ),
+              Text(
+                'Senior Java Developer',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.tealAccent,
                   ),
-                ),
-                Text(
-                  'Senior Java Developer',
-                  style: TextStyle(
-                    fontFamily: 'Source Sans Pro',
-                    fontSize: 20.0,
-                    color: Colors.teal.shade100,
-                    letterSpacing: 2.5,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Center(
-                  child: Card(
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10.0,
-                      horizontal: 25.0,
-                    ),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.phone,
-                        color: Colors.tealAccent,
-                      ),
-                      title: Text(
-                        '+964 - 7502078246',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 20.0,
-                        ),
-                      ),
+                  title: Text(
+                    '+964 - 7502078246',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
                     ),
                   ),
                 ),
-                Center(
-                  child: Card(
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10.0,
-                      horizontal: 25.0,
-                    ),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.email,
-                        color: Colors.tealAccent,
-                      ),
-                      title: Text(
-                        'mehmet.anbaki@gmail.com',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 20.0,
-                        ),
-                      ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.tealAccent,
+                  ),
+                  title: Text(
+                    'mehmet.anbaki@gmail.com',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
